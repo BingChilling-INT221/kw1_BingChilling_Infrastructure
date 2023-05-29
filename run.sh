3af736d707b0a7f7a269d90fec29a2824b58c273
Cn-niyom <chalnthorn.niyom@gmail.com>
2023-05-29 15:24:33 +0700

    add run sh file

---
+++ run.sh
@@ -0,0 +1,9 @@
+ #! /bin/sh
+cd /home/moniyom/kw1_BingChilling_Frontend/
+git pull
+cd /home/moniyom/kw1_BingChilling_Backend/
+git pull
+cd /home/moniyom/kw1_BingChilling_Infrastructure/
+docker compose build
+docker compose up -d
+docker system prune --all --volumes
